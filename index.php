b2d8bfca870004eb5c8389a1e116b2f9cf3df779
Future Computer Training Institute <107338637+fctiadmin@users.noreply.github.com>
2023-05-24 13:09:35 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+if(isset($_POST['save'])){
+	$sname=$_POST['sname'];
+	$fname=$_POST['fname'];
+	$mname=$_POST['mname'];
+	$class=$_POST['class'];
+	echo $sname.'<br>';
+	echo $fname .'<br>';
+	echo $mname .'<br>';
+	echo $class .'<br>';
+}
+?>
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<form action="" method="post">
+			
+			
+			<input type="text" placeholder="Name" name="sname"/><br />
+			<input type="text" placeholder="Father Name" name="fname" /><br />
+			<input type="text" placeholder="Mother Name" name="mname" /><br />
+			<input type="text" placeholder="Class" name="class" /><br />
+			<input type="submit" value="Submit" name="save" /><br />
+	
+	
+	</form>
+</body>
+</html>
